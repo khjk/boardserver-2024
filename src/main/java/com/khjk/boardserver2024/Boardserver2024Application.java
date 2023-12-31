1a336562f5438f4b1aea0bf6e52365e4beaadf4c
강효정 <begywjd15@flow.team>
2023-12-31 16:43:05 +0900

    initial commit

---
+++ src/main/java/com/khjk/boardserver2024/Boardserver2024Application.java
@@ -0,0 +1,13 @@
+package com.khjk.boardserver2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boardserver2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boardserver2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/khjk/boardserver2024/Boardserver2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khjk.boardserver2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boardserver2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
